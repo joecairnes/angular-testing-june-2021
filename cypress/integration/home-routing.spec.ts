e4e844d1cfca41f1b4cf52f2bb38051451aeee97
Jeffry Gonzalez <jeff@hypertheory.com>
2021-06-07 16:46:40 -0400

    really done

--- cypress/integration/home-routing.spec.ts
+++ cypress/integration/home-routing.spec.ts
@@ -4,15 +4,11 @@ describe('routing from the root', () => {
     cy.visit('/');
   });
   it('can go to converter', () => {
-    //cy.goto('converter');
-
     cy.findRouterLinkFor('converter').click();
-    //cy.get('[data-router-link=converter]').click();
     cy.get('[data-temp-converter]');
   });
 
   it('can go home', () => {
-    // cy.get('[data-router-link=home]').click();
     cy.findRouterLinkFor('home').click();
     cy.get('[data-home]');
   });
