9fcb67347f49c5e605b41642b3930b8625b43787
Jeffry Gonzalez <jeff@hypertheory.com>
2021-06-07 12:35:39 -0400

    before lunch

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-testing');
+    expect(app.title).toEqual('Angular Testing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-testing app is running!');
+    expect(compiled.querySelector('header>h1').textContent).toContain('Angular Testing');
   });
 });
---
+++ src/app/components/temp-converter/temp-converter.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-converter',
+  templateUrl: './temp-converter.component.html',
+  styleUrls: ['./temp-converter.component.css']
+})
+export class TempConverterComponent {
+
+
+  convertedTemp: number | undefined;
+
+  constructor() {
+  }
+
+
+  convertToC(temp: number) {
+    this.convertedTemp = 100;
+  }
+
+}
